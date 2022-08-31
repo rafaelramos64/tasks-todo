994d87bd4ea6870bdf7b4e3f0625ea1564814587
rafaelramos64 <rafaelmaggot64@gmail.com>
2022-08-31 17:18:45 -0300

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs'
+
+const api = 'http://localhost:5000'
 
 function App() {
+  const [title, setTitle] = useState('')
+  const [time, setTime] = useState('')
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true)
+
+      const res = await fetch(api + '/todos')
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch(err => console.error(err))
+
+        setLoading(false)
+        setTodos(res)
+    }
+
+    loadData()
+  }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    }
+
+    await fetch(api + '/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    setTodos((prevState) =>  [...prevState, todo])
+
+    setTitle('')
+    setTime('')
+  }
+
+  const handleDelete = async (id) => {
+    await fetch(`${api}/todos/${id}`, {
+      method: 'DELETE',
+    })
+
+    setTodos((prevState) => [...prevState.filter((todo) => todo.id !== id)])
+  }
+
+  const handleEdit = async(todo) => {
+    todo.done = !todo.done
+
+    const data = await fetch(`${api}/todos/${todo.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    setTodos((prevState) => 
+      prevState.map((todo) => (todo.id === data.id ? (todo = data) : todo))
+    )
+  }
+
+  if (loading) return <p> Carregando... </p>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1> rafael </h1>
+      </div>
+
+      <div className="todo-form">
+        <h2> Insira a sua próxima tarefa </h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title"> O que você vai fazer? </label>
+
+            <input
+              className=""
+              type="text"
+              placeholder="Título da tarefa"
+              onChange={(event) => setTitle(event.target.value)}
+              value={title || ''}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="time"> Duração: </label>
+
+            <input
+              className=""
+              type="text"
+              name="time"
+              placeholder="Tempo estimado (em horas)"
+              onChange={(event) => setTime(event.target.value)}
+              value={time || ''}
+              required
+            />
+          </div>
+  
+          <button className="form-btn" type="submit" > Salvar</button>
+        </form>
+      </div>
+
+      <div className="todo-list">
+        <h2>Lista de tarefas: </h2>
+        { todos.length === 0 && <p>Não há tarefas</p> }
+
+        { todos.map((todo) => (
+            <div className="todo" key={todo.id}>
+              <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+
+              <p>Duração: { todo.time }</p>
+
+              <div className="actions">
+                <span onClick={() => handleEdit(todo)}> { !todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}</span>
+                <BsTrash onClick={() => handleDelete(todo.id)}/>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
